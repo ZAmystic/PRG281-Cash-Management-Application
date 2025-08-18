715dd093eb8ac4399c2daa3d6b1b2ab7915c3e39
Theart <theartjooste05@gmail.com>
2025-08-18 21:12:14 +0200

    Add files via upload

---
+++ Cash Management Console Application/PRG281_Project/PRG281_Project/ASCII.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG281_Project
+{
+    class ASCII
+    {
+        public void IncomeModuleDisplay()
+        {
+            //Displays ASCII art for the income module to allow the user to see what module they are in
+            //This improves user experience by providing a visual representation of the module
+            Console.WriteLine("-------------------------------------------------------------------------");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(@"  _____                                  __  __           _       _      
+ |_   _|                                |  \/  |         | |     | |     
+   | |  _ __   ___ ___  _ __ ___   ___  | \  / | ___   __| |_   _| | ___ 
+   | | | '_ \ / __/ _ \| '_ ` _ \ / _ \ | |\/| |/ _ \ / _` | | | | |/ _ \
+  _| |_| | | | (_| (_) | | | | | |  __/ | |  | | (_) | (_| | |_| | |  __/
+ |_____|_| |_|\___\___/|_| |_| |_|\___| |_|  |_|\___/ \__,_|\__,_|_|\___|");
+            Console.ResetColor();
+            Console.WriteLine("-------------------------------------------------------------------------");
+        }
+
+        public void ExpenseModuleDisplay()
+        {
+            //Displays ASCII art for the expense module to allow the user to see what module they are in
+            //This improves user experience by providing a visual representation of the module
+            Console.WriteLine("---------------------------------------------------------------------------");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(@"  ______                                  __  __           _       _      
+ |  ____|                                |  \/  |         | |     | |     
+ | |__  __  ___ __   ___ _ __  ___  ___  | \  / | ___   __| |_   _| | ___ 
+ |  __| \ \/ / '_ \ / _ \ '_ \/ __|/ _ \ | |\/| |/ _ \ / _` | | | | |/ _ \
+ | |____ >  <| |_) |  __/ | | \__ \  __/ | |  | | (_) | (_| | |_| | |  __/
+ |______/_/\_\ .__/ \___|_| |_|___/\___| |_|  |_|\___/ \__,_|\__,_|_|\___|
+             | |                                                          
+             |_|                                                          ");
+            Console.ResetColor();
+            Console.WriteLine("---------------------------------------------------------------------------");
+        }
+
+        public void CashModuleDisplay()
+        {
+            //Displays ASCII art for the net cash module to allow the user to see what module they are in
+            //This improves user experience by providing a visual representation of the module
+            Console.WriteLine("------------------------------------------------------------");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(@"   _____          _       __  __           _       _      
+  / ____|        | |     |  \/  |         | |     | |     
+ | |     __ _ ___| |__   | \  / | ___   __| |_   _| | ___ 
+ | |    / _` / __| '_ \  | |\/| |/ _ \ / _` | | | | |/ _ \
+ | |___| (_| \__ \ | | | | |  | | (_) | (_| | |_| | |  __/
+  \_____\__,_|___/_| |_| |_|  |_|\___/ \__,_|\__,_|_|\___|");
+            Console.ResetColor();
+            Console.WriteLine("------------------------------------------------------------");
+        }
+
+        public void CashStatsModuleDisplay()
+        {
+            //Displays ASCII art for the net cash module to allow the user to see what module they are in
+            //This improves user experience by providing a visual representation of the module
+            Console.WriteLine("--------------------------------------------------------------------");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine(@"   _____          _        _____ _        _   _     _   _          
+  / ____|        | |      / ____| |      | | (_)   | | (_)         
+ | |     __ _ ___| |__   | (___ | |_ __ _| |_ _ ___| |_ _  ___ ___ 
+ | |    / _` / __| '_ \   \___ \| __/ _` | __| / __| __| |/ __/ __|
+ | |___| (_| \__ \ | | |  ____) | || (_| | |_| \__ \ |_| | (__\__ \
+  \_____\__,_|___/_| |_| |_____/ \__\__,_|\__|_|___/\__|_|\___|___/");
+            Console.ResetColor();
+            Console.WriteLine("--------------------------------------------------------------------");
+        }
+    }
+}
---
+++ Cash Management Console Application/PRG281_Project/PRG281_Project/CashModule.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRG281_Project
+{
+    class CashMain
+    {
+        ASCII Ascii = new ASCII();
+
+        private static double amount;
+        protected internal static double[] arrIncome = new double[500];
+        protected internal static int IncomeCounter = 0;
+        protected internal static double[] arrExpense = new double[500];
+        protected internal static int ExpenseCounter = 0;
+
+        public static double Amount
+        {
+            get // Encapsulation for amount variable
+            {
+                return amount;
+            }
+            set
+            {
+                amount = value;
+            }
+        }
+
+        public static double AmountValue
+        {
+            get { return amount; }
+        }
+    }
+
+    class AddCash : CashMain
+    {
+        public void AddIncome()
+        {
+            ASCII display = new ASCII();
+            bool continueAdding = true;
+
+            display.IncomeModuleDisplay();
+
+            while (continueAdding)
+            {
+                double income;
+
+                Console.WriteLine("Please enter the amount of cash to add:");
+                if (!double.TryParse(Console.ReadLine(), out income) || income < 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid amount. Please enter a positive number.");
+                    Console.ResetColor();
+                    continue;
+                }
+
+                Amount += income;
+                arrIncome[IncomeCounter] = income;
+                IncomeCounter++;
+
+                Console.Write("Income added: ");
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.Write($"{income:C}");
+                Console.ResetColor();
+                Console.Write(". Total amount: ");
+
+                if (Amount < 0)
+                    Console.ForegroundColor = ConsoleColor.Red;
+                else if (Amount > 0)
+                    Console.ForegroundColor = ConsoleColor.Green;
+                else
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+
+                Console.WriteLine($"{Amount:C}");
+                Console.ResetColor();
+
+                Console.Write("Would you like to add more cash? ");
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("[Y/N]");
+                Console.ResetColor();
+
+                string input = Console.ReadLine().Trim().ToUpper();
+                continueAdding = (input == "Y");
+            }
+        }
+    }
+
+    class SubtractCash : CashMain
+    {
+        public void AddExpenses()
+        {
+            ASCII display = new ASCII(); // Instantiate the ASCII class to display the module art
+            bool continueAdding = true;
+            const double VAT = 0.15; // Constant VAT rate of 15%
+
+            display.ExpenseModuleDisplay(); // Display ASCII art for the expense module
+
+            while (continueAdding)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine("                   Is this purchase VAT-inclusive? [Y/N]                   ");
+                Console.ResetColor();
+                Console.WriteLine("---------------------------------------------------------------------------");
+                Console.ForegroundColor = ConsoleColor.DarkGray;
+                Console.WriteLine("            Y - will automatically add VAT to the amount entered           ");
+                Console.WriteLine("            N - will ensure the purchase was not VAT-inclusive             ");
+                Console.ResetColor();
+                Console.WriteLine("---------------------------------------------------------------------------");
+
+                string inputVAT = Console.ReadLine().Trim().ToUpper();
+                Console.WriteLine("");
+
+                double finalExpense;
+                double expenses;
+
+                Console.WriteLine("Please enter the amount of cash expenses:");
+                if (!double.TryParse(Console.ReadLine(), out expenses) || expenses < 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid amount. Please enter a positive number.");
+                    Console.ResetColor();
+                    continue;
+                }
+
+                if (inputVAT == "Y")
+                {
+                    finalExpense = expenses * (1 + VAT);
+                }
+                else if (inputVAT == "N")
+                {
+                    finalExpense = expenses;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Invalid VAT option. Please enter Y or N.");
+                    Console.ResetColor();
+                    continue;
+                }
+
+                Amount -= finalExpense;
+                arrExpense[ExpenseCounter] = finalExpense;
+                ExpenseCounter++;
+
+                Console.Write("Expenses added: ");
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.Write($"-{finalExpense:C}");
+                Console.ResetColor();
+                Console.Write(". Total amount: ");
+
+                if (Amount < 0)
+                    Console.ForegroundColor = ConsoleColor.Red;
+                else if (Amount > 0)
+                    Console.ForegroundColor = ConsoleColor.Green;
+                else
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+
+                Console.WriteLine($"{Amount:C}");
+                Console.ResetColor();
+
+                Console.Write("Would you like to add more expenses? ");
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("[Y/N]");
+                Console.ResetColor();
+
+                string input = Console.ReadLine().Trim().ToUpper();
+                continueAdding = (input == "Y");
+            }
+        }
+    }
+
+    class CalculateCash : CashMain
+    {
+        public void CalculateTotalCash()
+        {
+            ASCII display = new ASCII(); // Instantiate the ASCII class to display the module art
+            display.CashModuleDisplay(); // Display ASCII art for the net cash module
+
+            Console.Write("The current amount of net cash is: ");
+
+            if (Amount < 0)
+                Console.ForegroundColor = ConsoleColor.Red;
+            else if (Amount > 0)
+                Console.ForegroundColor = ConsoleColor.Green;
+            else
+                Console.ForegroundColor = ConsoleColor.Yellow;
+
+            Console.WriteLine($"{Amount:C}");
+            Console.ResetColor();
+        }
+    }
+
+    class CashStatistics : CashMain
+    {
+        public void displayCashCharts()
+        {
+            Console.Clear();
+            ASCII display = new ASCII();
+            display.CashStatsModuleDisplay();
+
+            bool hasIncome = IncomeCounter > 0;
+            bool hasExpense = ExpenseCounter > 0;
+
+            if (!hasIncome && !hasExpense)
+            {
+                Console.WriteLine("No data available to display charts.");
+                return;
+            }
+
+            int maxEntries = Math.Max(IncomeCounter, ExpenseCounter);
+            double maxIncome = hasIncome ? arrIncome.Take(IncomeCounter).Max() : 0;
+            double maxExpense = hasExpense ? arrExpense.Take(ExpenseCounter).Max() : 0;
+            double maxValue = Math.Max(maxIncome, maxExpense);
+
+            if (hasIncome)
+            {
+                Console.WriteLine("Income Chart:");
+                for (int i = 0; i < IncomeCounter; i++)
+                {
+                    DrawBar(arrIncome[i], maxValue, ConsoleColor.Green, i);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No income data available.");
+            }
+
+            if (hasExpense)
+            {
+                Console.WriteLine("Expense Chart:");
+                for (int i = 0; i < ExpenseCounter; i++)
+                {
+                    DrawBar(arrExpense[i], maxValue, ConsoleColor.Red, i);
+                }
+            }
+            else
+            {
+                Console.WriteLine("\n No expense data available.");
+            }
+        }
+
+
+        private static void DrawBar(double value, double maxValue, ConsoleColor color, int index)
+        {
+            int barWidth = 50;
+            int scaledLength = (int)((value / maxValue) * barWidth); //convert to int
+
+            Console.ForegroundColor = color;
+            Console.Write(new string('█', scaledLength));            //convert to string
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine($" {value:C}");                        //displays value next to bar
+        }
+
+    }
+}
